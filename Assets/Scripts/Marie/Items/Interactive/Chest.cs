8e48b3bb489b852c2b60b79afe187a4ee31c61d6
L3o <74620207+leorchr@users.noreply.github.com>
2022-11-23 19:39:52 +0100

    [feat] add animation to open chest

--- Assets/Scripts/Marie/Items/Interactive/Chest.cs
+++ Assets/Scripts/Marie/Items/Interactive/Chest.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class Chest : Interactive
 {
     public KeyItemData key;
+    public Animator animator;
     public override void OnInteraction()
     {
         //If I want to do the base OnInteraction anyway first
         //
         //Activate light and fire
+        animator.SetBool("chestOpen", true);
         Inventory.Instance.PickupKeyItem(key);
     }
 }
